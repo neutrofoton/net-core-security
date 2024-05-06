ee9974aff2f70f46e9394549e56045aca5a7954c
neutro <neutro@c13.io>
2024-05-06 16:50:20 +0700

    add redis

--- src/NetSSO/NetSSO.App1/Program.cs
+++ src/NetSSO/NetSSO.App1/Program.cs
@@ -2,10 +2,19 @@ using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.DataProtection;
+using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//add data protection
+var redisAddress = builder.Configuration.GetValue<string>("Redis:IpPort");
+var redisPassword = builder.Configuration.GetValue<string>("Redis:Password");
+
+var options = ConfigurationOptions.Parse(redisAddress!); // host1:port1, host2:port2, ...
+options.Password = redisPassword;      
+
 builder.Services.AddDataProtection()
+    .PersistKeysToStackExchangeRedis(ConnectionMultiplexer.Connect(options))
     .SetApplicationName("neutro");
 
 builder.Services.AddAuthorization();
